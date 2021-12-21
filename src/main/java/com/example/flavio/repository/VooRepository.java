8356948e3b54cb5f4a6f87d0f7c1a653e38b6d93
Flaviogomes1 <91925045+Flaviogomes1@users.noreply.github.com>
2021-12-21 18:40:56 -0300

    Add files via upload

---
+++ src/main/java/com/example/flavio/repository/VooRepository.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.flavio.repository;
+
+import com.example.flavio.models.Voo;
+import com.example.flavio.models.Voo;
+import java.time.LocalDate;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ *
+ * @author flavio
+ */
+public interface VooRepository extends JpaRepository<Voo, Long> {
+
+    @Query("from Voo v where v.origem.id = ?1 AND v.destino.id=?2")
+    public List<Voo> listVooOrigem(long origem, long destino);
+
+    @Query("from Voo v where v.origem.id = ?1 AND v.destino.id=?2 AND v.datapartida = ?3")
+    public List<Voo> listVooData(long origem, long destino, LocalDate date);
+
+    @Query("from Voo v where v.origem.id = ?1 AND v.destino.id=?2 AND v.datapartida = ?3 AND v.preco = ?4")
+    public List<Voo> listVooPreco(long origem, long destino, LocalDate date, Double preco);
+
+    @Override
+    public Voo save(Voo voo);
+
+    
+
+}
